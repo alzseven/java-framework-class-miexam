74f547e8d8d3067b224037b6df418a6dee1a7315
alzseven <jhp7140@gmail.com>
2021-04-23 21:39:22 +0900

    Extract and move lamdas to Context

--- src/main/java/kr/ac/jejunu/userdao/JdbcContext.java
+++ src/main/java/kr/ac/jejunu/userdao/JdbcContext.java
@@ -1,10 +1,7 @@
 package kr.ac.jejunu.userdao;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JdbcContext {
 
@@ -97,5 +94,39 @@ public class JdbcContext {
                 e.printStackTrace();
             }
         }
+    }
+
+
+    public void update(String sql, Object[] params) throws SQLException {
+        JdbcContextUpdate(connection -> {
+                    PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                    for (int i = 0; i < params.length; i++) {
+                        preparedStatement.setObject(i + 1, params[i]);
+                    }
+                    return preparedStatement;
+                }
+        );
+    }
+
+    public void insert(User user, String sql, Object[] params) throws SQLException {
+        JdbcContextInsert(user, connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql,
+                    Statement.RETURN_GENERATED_KEYS
+            );
+            for (int i = 0; i < params.length; ++i) {
+                preparedStatement.setObject(i + 1, params[i]);
+            }
+            return preparedStatement;
+        });
+    }
+
+    public User findById(String sql, Object[] params) throws SQLException {
+        return JdbcContextFindbyId(connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            for (int i = 0; i < params.length; i++) {
+                preparedStatement.setObject(i + 1, params[i]);
+            }
+            return preparedStatement;
+        });
     }
 }
--- src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -12,57 +12,24 @@ public class UserDao {
     public User findById(Integer id) throws SQLException {
         String sql = "select * from userinfo where id = ?";
         Object[] params = new Object[]{id};
-        return findById(sql, params);
-    }
-
-    public User findById(String sql, Object[] params) throws SQLException {
-        return jdbcContext.JdbcContextFindbyId(connection -> {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            for (int i = 0; i < params.length; i++) {
-                preparedStatement.setObject(i + 1, params[i]);
-            }
-            return preparedStatement;
-        });
+        return jdbcContext.findById(sql, params);
     }
 
     public void insert(User user) throws SQLException {
         String sql = "insert into userinfo(name,password) values (?,?) ";
         Object[] params = new Object[]{user.getName(), user.getPassword()};
-        jdbcContext.JdbcContextInsert(user, connection -> {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql,
-                    Statement.RETURN_GENERATED_KEYS
-            );
-            for (int i = 0; i < params.length; ++i) {
-                preparedStatement.setObject(i + 1, params[i]);
-            }
-            return preparedStatement;
-        });
+        jdbcContext.insert(user, sql, params);
     }
 
     public void update(User user) throws SQLException {
         String sql = "update userinfo set name =?,password =? where id = ? ";
         Object[] params = new Object[]{user.getId(), user.getName(), user.getPassword()};
-        jdbcContext.JdbcContextUpdate(connection -> {
-                    PreparedStatement preparedStatement = connection.prepareStatement(sql);
-                    for (int i = 0; i < params.length; i++) {
-                        preparedStatement.setObject(i + 1, params[i]);
-                    }
-                    return preparedStatement;
-                }
-        );
+        jdbcContext.update(sql, params);
     }
 
     public void delete(Integer id) throws SQLException {
         String sql = "delete from userinfo where id = ?";
         Object[] params = new Object[]{id};
-        jdbcContext.JdbcContextUpdate(connection -> {
-                    PreparedStatement preparedStatement =
-                            connection.prepareStatement(sql);
-                    for (int i = 0; i < params.length; i++) {
-                        preparedStatement.setObject(i + 1, params[i]);
-                    }
-                    return preparedStatement;
-                }
-        );
+        jdbcContext.update(sql, params);
     }
 }
