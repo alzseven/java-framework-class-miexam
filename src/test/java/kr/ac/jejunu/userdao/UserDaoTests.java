58a7d0fa5e5bb39a874412a29bffed519e4577e2
alzseven <jhp7140@gmail.com>
2021-04-23 16:47:57 +0900

    Add Insert

--- src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
+++ src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
@@ -15,9 +16,27 @@ public class UserDaoTests {
         String name = "JHP";
         String password = "JHPPW";
         UserDao userDao = new UserDao();
-        User user = userDao.get(id);
+        User user = userDao.findById(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
         assertThat(user.getPassword(), is(password));
+    }
+
+    @Test
+    public void insert() throws SQLException, ClassNotFoundException {
+        String name = "JHP";
+        String password = "1234";
+
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+
+        UserDao userDao = new UserDao();
+        userDao.insert(user);
+        User insertedUser = userDao.findById(user.getId());
+
+        assertThat(insertedUser.getId(), greaterThan(0));
+        assertThat(insertedUser.getName(), is(user.getName()));
+        assertThat(insertedUser.getPassword(), is(user.getPassword()));
     }
 }
